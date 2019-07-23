a84feb181f22bd3af99c42b12efaaa0e22925447
bakenray <lpqstrong@163.com>
2019-07-23 17:36:06 +0800

    first commit

---
+++ lesson-2/app.js
@@ -0,0 +1,30 @@
+var express = require('express')
+var utility = require('utility')
+
+var app = express()
+app.get('/',function(req,res){
+    console.log(req.query)
+    var q = req.query.q
+    var md5Value = utility.md5(q)
+    res.send(md5Value)
+})
+app.listen(3000,function(){
+    console.log('app is running at port 3000')
+})
+
+
+// 挑战，输入sha1值
+
+// var express = require('express')
+// var utility = require('utility')
+
+// var app = express()
+
+// app.get('/',function(req,res){
+//     let q = req.query.q
+//     var sha1Value = utility.sha1(q)
+//     res.send(sha1Value)
+// })
+// app.listen(3000,function(){
+//     console.log('listeing at 3000ss')
+// })
